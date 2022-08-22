2fdb5e374c08b8337281eb72238b0c96e0a0f985
KP <kp@cornerstonews>
2022-08-22 10:15:11 -0700

    Updated dependencies
    * Updated JWT library version and updated deprecated method calls to newer methods.

--- src/main/java/com/github/cornerstonews/webservice/jwt/JWTTokenUtil.java
+++ src/main/java/com/github/cornerstonews/webservice/jwt/JWTTokenUtil.java
@@ -24,6 +24,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtBuilder;
@@ -37,9 +40,8 @@ public class JWTTokenUtil {
     private final String secretKey;
     private final Key key;
     private final SignatureAlgorithm algorithm;
-    
+
     private static final Charset charset = Charset.forName("UTF-8");
-    
 
     public JWTTokenUtil(String secretKey) {
         this.secretKey = secretKey;
@@ -52,7 +54,7 @@ public class JWTTokenUtil {
         this.algorithm = SignatureAlgorithm.RS512;
         this.secretKey = null;
     }
-    
+
     public String generateToken(JWTPrincipal principal, Date expiration, Map<String, Object> claims) {
         JwtBuilder jwtBuilder = Jwts.builder()
                 .setSubject(principal.getName())
@@ -61,60 +63,61 @@ public class JWTTokenUtil {
                 .claim(USER, principal.getName())
                 .claim(ROLE, principal.getRoles())
                 .addClaims(claims);
-        
+
         if (this.secretKey != null) {
-            jwtBuilder.signWith(this.algorithm, this.secretKey.getBytes(charset));
+            SecretKey key = new SecretKeySpec(this.secretKey.getBytes(charset), this.algorithm.getJcaName());
+            jwtBuilder.signWith(key, this.algorithm);
         } else {
-            jwtBuilder.signWith(this.algorithm, key);
+            jwtBuilder.signWith(key);
         }
-        
+
         return jwtBuilder.compact();
     }
 
     public JWTPrincipal validateToken(String jwtToken) throws JWTException {
         Jws<Claims> claims = null;
         if (this.secretKey != null) {
-            claims = Jwts.parser().setSigningKey(this.secretKey.getBytes(charset)).parseClaimsJws(jwtToken);
+            claims = Jwts.parserBuilder().setSigningKey(this.secretKey.getBytes(charset)).build().parseClaimsJws(jwtToken);
         } else {
-            claims = Jwts.parser().setSigningKey(this.key).parseClaimsJws(jwtToken);
+            claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwtToken);
         }
-        
-        if(!this.algorithm.getValue().equals(claims.getHeader().getAlgorithm())) {
+
+        if (!this.algorithm.getValue().equals(claims.getHeader().getAlgorithm())) {
             throw new JWTException("Invalid token algorithm.");
         }
-        
+
         Claims body = claims.getBody();
-        
+
         if (new Date().after(body.getExpiration())) {
             throw new JWTException("Token has expired.");
         }
-        
+
         String username = body.getSubject();
         @SuppressWarnings("unchecked")
         ArrayList<String> roles = (ArrayList<String>) body.get(ROLE);
-        
+
         return new JWTPrincipal(username, roles);
     }
-    
+
     public Object getClaim(String jwtToken, Object keyName) {
-        Jws<Claims> claims = Jwts.parser().setSigningKey(this.secretKey.getBytes(charset)).parseClaimsJws(jwtToken);
+        Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(this.secretKey.getBytes(charset)).build().parseClaimsJws(jwtToken);
         Claims body = claims.getBody();
-        
+
         return body.get(keyName);
     }
-    
+
     public Map<String, Object> getClaims(String jwtToken, List<String> claimKeys) {
         Map<String, Object> claims = new HashMap<>();
-        
-        Jws<Claims> jwtClaims = Jwts.parser().setSigningKey(this.secretKey.getBytes(charset)).parseClaimsJws(jwtToken);
+
+        Jws<Claims> jwtClaims = Jwts.parserBuilder().setSigningKey(this.secretKey.getBytes(charset)).build().parseClaimsJws(jwtToken);
         Claims body = jwtClaims.getBody();
-        for(String claimKey : claimKeys) {
+        for (String claimKey : claimKeys) {
             Object value;
-            if((value = body.get(claimKey)) != null) {
+            if ((value = body.get(claimKey)) != null) {
                 claims.put(claimKey, value);
             }
         }
-        
+
         return claims;
     }
 }
